9a2f9d35792f4caaba3f054bfe17929486efcbe0
Turdaliyev Abdurahmon <121414053+Abdurahmon086@users.noreply.github.com>
2024-04-26 16:19:38 +0500

    api connect

--- App.js
+++ App.js
@@ -1,30 +1,41 @@
-import { StyleSheet } from 'react-native';
+import { Alert } from 'react-native';
 import Loader from './components/Loader';
 import Weather from './components/Weather';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import * as Location from 'expo-location';
 
 export default function App() {
+  const API_KEY = 'a44a3695dd97514eedaa3d29f4d628b7'
+
   const [visible, setVisible] = useState(false);
+  const [location, setLocation] = useState(null)
+
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    setLocation(data)
+    setVisible(!visible);
+  }
+
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        Alert.alert('Permission to access location was denied');
+        return;
+      }
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({});
+      getWeather(latitude, longitude)
+
+    } catch (err) {
+      Alert.alert('I can\'t find your location, so bad', err)
+    }
+  }
+
   useEffect(() => {
-    setInterval(() => {
-      setVisible(!visible);
-    }, 2000);
+    getLocation()
   }, []);
 
   return visible ? <Weather /> : <Loader />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  hello: {
-    fontSize: 50,
-    color: 'white',
-    textAlign: 'center',
-    margin: 10,
-  },
-});
